0f421cbb9fe043a813a9e5e55a4b7a8fbc7adc8f
makschernetskyi <konomerubokkusu@gmail.com>
2025-08-18 05:06:35 +0200

    update loader

--- widget_loader.js
+++ widget_loader.js
@@ -20,12 +20,12 @@
         })
         .catch(err => console.error("Formamind widget JS failed:", err));
 
-        // Load CSS via <link>
-        const cssUrl = `${workerBase}/css?key=${encodeURIComponent(window.FMD_ID)}`;
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = cssUrl;
-        document.head.appendChild(link);
+        // // Load CSS via <link>
+        // const cssUrl = `${workerBase}/css?key=${encodeURIComponent(window.FMD_ID)}`;
+        // const link = document.createElement("link");
+        // link.rel = "stylesheet";
+        // link.href = cssUrl;
+        // document.head.appendChild(link);
 
     } catch (e) {
         console.error("Formamind loader error:", e);
